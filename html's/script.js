f452402487daa0c9678fc0c4375a417f33440335
alberto <albertojmichael@gmail.com>
2026-07-13 12:34:01 -0300

    sd

--- html's/script.js
+++ html's/script.js
@@ -1,3 +1,9 @@
 function trocarpalavra() {
     document.getElementById("green-green-grass-of-home").innerHTML = 'verde verde grama da casa';
 }
+
+function modoEscuro() {
+  document.body.style.backgroundColor = "#222222";
+
+  document.getElementById("green-green-grass-of-home").style.color = white
+}
